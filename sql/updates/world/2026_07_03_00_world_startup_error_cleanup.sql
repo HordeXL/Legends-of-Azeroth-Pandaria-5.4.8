e89ae475b8279d6ddd5080af09ffe7e66034df26
ingussuveiks-dev <ingussuveiks@gmail.com>
2026-07-03 15:12:08 +0300

    izņemtas 8 konkrētās Forest Moth / Amber Moth avota spawn rindas, kas runtime laikā ģenerēja Z: -100000/-200000; SmartScript Entry 5649 noņemts nepareizais Normal Dungeon instance flags, tāpēc eventam vairs nevajadzētu tikt atmestam.

--- sql/updates/world/2026_07_03_00_world_startup_error_cleanup.sql
+++ sql/updates/world/2026_07_03_00_world_startup_error_cleanup.sql
@@ -23,3 +23,23 @@ DELETE FROM `creature_addon` WHERE `guid` IN (505704, 511897);
 DELETE FROM `creature` WHERE `guid` IN (505704, 511897)
   AND `id` = 40217
   AND `position_z` < -100000;
+
+-- Remove wild battle pet source spawns that produce invalid runtime Z coordinates.
+DELETE FROM `creature_addon` WHERE `guid` IN (
+  240186, 240214, 240261,
+  527565, 527567, 527568, 527577, 527582
+);
+
+DELETE FROM `creature` WHERE `guid` IN (
+  240186, 240214, 240261,
+  527565, 527567, 527568, 527577, 527582
+)
+  AND `id` IN (49842, 63980);
+
+-- Let this outdoor SmartAI event load instead of being filtered by instance difficulty flags.
+UPDATE `smart_scripts`
+SET `event_flags` = `event_flags` & 4294967293
+WHERE `entryorguid` = 5649
+  AND `source_type` = 0
+  AND `id` = 0
+  AND (`event_flags` & 2) <> 0;
