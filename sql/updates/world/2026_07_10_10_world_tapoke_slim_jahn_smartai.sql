47ed49b3eceab4cdda1e5a3d60b6fb25df51d10c
ingussuveiks-dev <ingussuveiks@gmail.com>
2026-07-10 11:21:52 +0300

    Atrastas un salabotas trīs veco spell rank ID nesaderības: Stealth 1785 → 1784; Stealth noņemšana 1785 → 1784; Backstab 2589 → 53.

--- sql/updates/world/2026_07_10_10_world_tapoke_slim_jahn_smartai.sql
+++ sql/updates/world/2026_07_10_10_world_tapoke_slim_jahn_smartai.sql
@@ -39,10 +39,10 @@ UPDATE `creature_template` SET `AIName`="SmartAI", `ScriptName`="" WHERE `entry`
 DELETE FROM `smart_scripts` WHERE `entryorguid`=4962 AND `source_type`=0;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=496200 AND `source_type`=9;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(4962,0,0,0,38,0,100,1,1,1,0,0,0,11,1785,0,0,0,0,0,1,0,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Data Set - Cast 'Stealth'"),
+(4962,0,0,0,38,0,100,1,1,1,0,0,0,11,1784,0,0,0,0,0,1,0,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Data Set - Cast 'Stealth'"),
 (4962,0,1,0,38,0,100,1,1,1,0,0,0,53,0,4962,0,0,0,2,1,0,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Data Set - Start Waypoint"),
 (4962,0,2,0,38,0,100,1,1,1,0,0,0,42,1,0,0,0,0,0,1,0,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Data Set - Set Invincibility at 1 HP"),
-(4962,0,3,0,40,0,100,0,3,4962,0,0,0,28,1785,0,0,0,0,0,1,0,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Waypoint 3 Reached - Remove Aura 'Stealth'"),
+(4962,0,3,0,40,0,100,0,3,4962,0,0,0,28,1784,0,0,0,0,0,1,0,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Waypoint 3 Reached - Remove Aura 'Stealth'"),
 (4962,0,4,0,40,0,100,0,3,4962,0,0,0,2,168,0,0,0,0,0,1,0,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Waypoint 3 Reached - Set Faction 168"),
 (4962,0,5,0,40,0,100,0,7,4962,0,0,0,6,1249,0,0,0,0,0,12,1,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Waypoint 7 Reached - Fail Quest 'The Missing Diplomat (Part 11)'"),
 (4962,0,6,0,40,0,100,0,7,4962,0,0,0,45,1,1,0,0,0,0,10,216482,0,0,0,0,0,0,"Tapoke \"Slim\" Jahn - On Waypoint 7 Reached - Set Data to Mikhail"),
@@ -65,7 +65,7 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=497100 AND `source_type`=9;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (4971,0,0,0,25,0,100,0,0,0,0,0,0,11,3616,0,0,0,0,0,1,0,0,0,0,0,0,0,"Slim's Friend - On Reset - Cast 'Poison Proc'"),
 (4971,0,1,0,25,0,100,0,0,0,0,0,0,11,8601,0,0,0,0,0,1,0,0,0,0,0,0,0,"Slim's Friend - On Reset - Cast 'Slowing Poison'"),
-(4971,0,2,0,67,0,100,0,3000,6000,0,0,0,11,2589,0,0,0,0,0,2,0,0,0,0,0,0,0,"Slim's Friend - On Behind Target - Cast Backstab"),
+(4971,0,2,0,67,0,100,0,3000,6000,0,0,0,11,53,0,0,0,0,0,2,0,0,0,0,0,0,0,"Slim's Friend - On Behind Target - Cast Backstab"),
 (4971,0,3,0,38,0,100,1,1,1,0,0,0,80,497100,2,0,0,0,0,1,0,0,0,0,0,0,0,"Slim's Friend - On Data Set - Run Script"),
 (497100,9,0,0,0,0,100,0,0,0,0,0,0,24,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Slim's Friend - On Script - Evade"),
 (497100,9,1,0,0,0,100,0,0,0,0,0,0,2,123,0,0,0,0,0,1,0,0,0,0,0,0,0,"Slim's Friend - On Script - Set Faction 123"),
