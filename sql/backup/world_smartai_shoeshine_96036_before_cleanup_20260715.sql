ca9431bd556a29a14967879cd8ae6ae914b2960c
ingussuveiks-dev <ingussuveiks@gmail.com>
2026-07-15 16:17:42 +0300

    īstais Shoeshine Seat template ir 194115; tā spawn GUID ir 73279; NPC Sheddle 29703 creature GUID ir 96036; kļūdainais 96036/9603600 SmartAI bija pilnīgs īstā scenārija dublikāts ar nepareizu identifikatoru

---
+++ sql/backup/world_smartai_shoeshine_96036_before_cleanup_20260715.sql
@@ -0,0 +1,6 @@
+-- Exact rows removed by 2026_07_15_04_world_remove_duplicate_shoeshine_smartai.sql
+INSERT IGNORE INTO `smart_scripts` VALUES
+(96036,1,0,0,64,0,100,0,0,0,0,0,0,80,9603600,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Shoeshine Seat - On Gossip hello - Action list'),
+(9603600,9,0,0,0,0,100,0,0,0,0,0,0,64,1,0,0,0,0,0,7,0,0,0,0,0,0,0,0,'Shoeshine Seat - Action list - Store target'),
+(9603600,9,1,0,0,0,100,0,0,0,0,0,0,100,1,0,0,0,0,0,19,29703,20,0,0,0,0,0,0,'Shoeshine Seat - Action list - Send Target'),
+(9603600,9,2,0,0,0,100,0,0,0,0,0,0,45,1,1,0,0,0,0,19,29703,20,0,0,0,0,0,0,'Shoeshine Seat - Action list - Set Data');
